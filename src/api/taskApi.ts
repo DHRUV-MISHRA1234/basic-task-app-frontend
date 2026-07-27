78a90f55438327f3da5fcbd1891d40af955e7a95
unknown <mishradhruv626@gmail.com>
2026-07-27 17:30:09 +0530

    New Changes

--- src/api/taskApi.ts
+++ src/api/taskApi.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { Task } from '../types/Task';
 
-const API_URL = 'http://localhost:8080/api/v2/tasks';
+// const API_URL = 'http://localhost:8080/api/v2/tasks';
+
+const API_URL = 'https://basic-task-app-backend-production.up.railway.app/api/v2/tasks';
 
 export const getTasks = async (): Promise<Task[]> => {
     const response = await axios.get(API_URL);
